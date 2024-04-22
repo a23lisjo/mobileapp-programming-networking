6d5435ab1aee090bec00f292d61e877347ce07ed
a23lisjo <a23lisjo@student.his.se>
2024-04-22 14:19:23 +0200

    Skapad recycle view adapter

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
@@ -11,12 +15,26 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
 
+    ArrayList<mountain> mountains= new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        /*ArrayList<mountain> mountains = new ArrayList<>();
+        mountains.add( new mountain("k2"));*/
+
+        RecyclerView recycler_view = findViewById(R.id.recycler_view);
+
+        recyclerviewadapter adapter = new recyclerviewadapter (this, mountains);
+        recycler_view.setAdapter(adapter);
+        recycler_view.setLayoutManager(new LinearLayoutManager(this));
+
         new JsonFile(this, this).execute(JSON_FILE);
+
     }
 
     @Override
