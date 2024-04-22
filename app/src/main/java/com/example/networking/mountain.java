8246bd543007fd0002f965b1c09a4bff0e082b06
a23lisjo <a23lisjo@student.his.se>
2024-04-22 14:18:40 +0200

    Skapad klass för mountain

---
+++ app/src/main/java/com/example/networking/mountain.java
@@ -0,0 +1,65 @@
+package com.example.networking;
+
+public class mountain {
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private int size;
+
+    public mountain(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    private int cost;
+
+
+}
