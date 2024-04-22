c4f863150ac06d56e63ca4b6bcd6346734ce3904
a23lisjo <a23lisjo@student.his.se>
2024-04-22 15:43:44 +0200

    Lagt till kod för att hämta, öppna, sist skriva ut data

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -7,43 +7,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
-    private final String JSON_FILE = "mountains.json";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
 
     ArrayList<mountain> mountains= new ArrayList<>();
-
     Gson gson = new Gson();
+    String json = gson.toJson(mountains);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        new JsonTask (this).execute(JSON_URL);
+
 
 
         /*ArrayList<mountain> mountains = new ArrayList<>();
         mountains.add( new mountain("k2"));*/
 
-        RecyclerView recycler_view = findViewById(R.id.recycler_view);
-
-        recyclerviewadapter adapter = new recyclerviewadapter (this, mountains);
-        recycler_view.setAdapter(adapter);
-        recycler_view.setLayoutManager(new LinearLayoutManager(this));
-
-        new JsonFile(this, this).execute(JSON_FILE);
 
     }
 
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
+
+        Type type = new TypeToken<ArrayList<mountain>>() {}.getType();
+        ArrayList<mountain> mountains = gson.fromJson(json, type);
+
+        RecyclerView recycler_view = findViewById(R.id.recycler_view);
+
+        recyclerviewadapter adapter = new recyclerviewadapter (this, mountains);
+        recycler_view.setAdapter(adapter);
+        recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        adapter.notifyDataSetChanged();
+
     }
 
 }
